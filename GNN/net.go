ec13dfdfd5d711de84d0928e933f633e27a5a75e
pehringer <jacob.ray.pehringer@gmail.com>
2024-01-09 21:47:03 -0700

    Docs.

---
+++ GNN/net.go
@@ -0,0 +1,13 @@
+/*
+A neural network parmater can be encoded as ordered pair.
+The ordered pair consist of an integer and decimal value.
+For example:
+             ( - 123 , +4.56 )                ( + 789 , -0.12 )
+               ^  ^      ^                      ^  ^      ^
+              /  /      /                      /  /      /
+Bias parameter  /      /       Weight parameter  /      /
+      For neuron      /               From neuron      /
+            With value                       With value
+
+A
+*/
