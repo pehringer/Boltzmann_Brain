f935bc22383444073e6923323c9138fd836dddf9
pehringer <jacob.ray.pehringer@gmail.com>
2024-01-13 23:57:01 -0700

    Better format.

--- GNN/net.go
+++ GNN/net.go
@@ -1,41 +1,57 @@
-/*
-A neural network parmater can be encoded as a ordered pair consisting of a
-integer and decimal value.
-
-               ( - 123 , +4.56 )                   ( + 789 , -0.12 )
-                 ^  ^      ^                         ^  ^      ^
-                /  /      /                         /  /      /
-  Bias parameter  /      /          Weight parameter  /      /
-        For neuron      /                  From neuron      /
-              With value                          With value
-
-
-A neural network neuron can be encoded as a series of parameters consisting
-of a bias parameter followed by zero to many weight parameters.
-                         _____                              _____
-  (-5, -1.94)  1________/  5  \     (-3, -0.51)            /  3  \
-  (+1, +0.73)    +0.73 /       \    (+5, +0.85)   5_______/       \
-  (+2, -0.68)  2_______\ +1.94 /                    +0.85 \ -0.51 /
-                 -0.68  \_____/                            \_____/
-
-A neural network can be encode as a series of neurons and its activation
-order is determined by the ordering of the neurons
-                 _____                 _____                 _____
-  (-5, +1.94)   /  1  \               /  5  \               /  3  \
-  (+1, +0.73)  /       \_____________/       \_____________/       \
-  (+2, -0.68)  \       /    +0.73   /\ +1.94 /\   +0.85    \ -0.51 /
-  (-3, -0.51)   \_____/            /  \_____/  \            \_____/
-  (+5, +0.85)              _______/             \_______
-  (-4, +2.21)    _____    / -0.68                 -0.36 \    _____
-  (+5, -0.36)   /  2  \  /                               \  /  4  \
-  (+2, +1.49)  /       \/_________________________________\/       \
-               \       /               +1.49               \ +2.21 /
-                \_____/                                     \_____/
-*/
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+/*
+A parmater can be encoded as a pair of values:
+
+             ( - # , #.# )
+               | |    |
+  Bias parameter |    |
+    For activation    |
+             With value
+
+               ( + # , #.# )
+                 | |    |
+  Weight parameter |    |
+     From activation    |
+               With value
+
+
+
+A network can be encoded as a list of parameters:
+
+  Activations                      |  Parameters
+  __[1]___[2]___[3]___[4]___[5]__  |  _____________
+                                   |
+  [ 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ]  |                   Set first N activations to N inputs [ A.A, A.A].
+                                   |
+                                   |                    _____________
+  [ A.A , A.A , 0.0 , 0.0 , 0.0 ]  |  (  2 ,  0.9 )    2   0.9  | 0.5\__   A series of weights followed
+                                   |  (  1 , -2.7 )     ________|____/  3  by a bias describes a neuron.
+                                   |  ( -3 ,  0.5 )    1  -2.7
+                                   |
+                                   |                             ____
+  [ A.A , A.A , B.B , 0.0 , 0.0 ]  |  (  3 ,  1.5 )     ________|-0.3\__   A series of weights followed
+                                   |  ( -5 , -0.3 )    3   1.5  |____/  5  by a bias describes a neuron.
+                                   |
+                                   |                    _____________
+  [ A.A , A.A , B.B , 0.0 , C.C ]  |  (  1 ,  1.1 )    1   1.1  | 0.8\__   A series of weights followed
+                                   |  (  2 , -0.3 )     ________|____/  4  by a bias describes a neuron.
+                                   |  ( -4 ,  0.8 )    2  -0.3
+                                   |
+                                   |
+                                   |  (  4 , -2.1 )    Since there is no bias parameter the result
+                                   |  (  1 , -0.3 )    of these weight calculations are not stored.
+                                   |
+                                   |
+  [ A.A , A.A , B.B , D.D , C.C ]  |                   Set N outputs from last N activations [ D.D, C.C].
+                                  _|_
+                                  \ /
+                                   '
+*/
 
 type ParameterPair struct {
 	metadata	int64
@@ -73,6 +89,13 @@ func (p ParameterPair) GetNeuron() int64 {
 
 func (p ParameterPair) GetValue() float64 {
 	return p.data
+}
+
+type NetworkList struct {
+	activation	[]float64
+	input		[]float64
+	output		[]float64
+	parameter	[]ParameterPair
 }
 
 func main() {
